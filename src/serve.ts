a26a12d4454e9a798832d560d9293a6ad9497cd6
webgently <oleksandrshkabura811@gmail.com>
2023-05-05 03:00:51 +0300

    dep: setting the prettier

--- src/serve.ts
+++ src/serve.ts
@@ -1,19 +1,19 @@
-import express, { Express } from "express";
-import cors from "cors";
+import express, { Express } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
 const port: Number = Number(process.env.HTTP_PORT || 2083);
 
 app.use(
-    cors({
-        origin: "*",
-        methods: ["POST", "GET"],
-    })
+  cors({
+    origin: '*',
+    methods: ['POST', 'GET']
+  })
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-    console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
