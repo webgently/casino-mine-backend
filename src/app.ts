6be849f2f40671e9ea47d51f7617720774a2d0b9
webgently <oleksandrshkabura811@gmail.com>
2023-05-05 03:22:14 +0300

    update: connect the db and structure the app

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
-import express, { Express } from 'express';
+import http from 'http';
+import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import { Server } from 'socket.io';
 
-const app: Express = express();
-const port: Number = Number(process.env.HTTP_PORT || 2083);
+import config from './config.json';
+import { setlog } from './helper';
+import { connect } from './model';
 
-app.use(
-  cors({
-    origin: '*',
-    methods: ['POST', 'GET']
-  })
-);
+process.on('uncaughtException', (error) => setlog('exception', error));
+process.on('unhandledRejection', (error) => setlog('rejection', error));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+const server = http.createServer(app);
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+connect().then(async (loaded) => {
+  if (loaded === true) {
+    setlog('connected to MongoDB');
+
+    app.use(cors({ origin: '*' }));
+    app.use(express.urlencoded());
+    app.use(bodyParser.json({ type: 'application/json' }));
+    app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+    app.use(bodyParser.text({ type: 'text/html' }));
+    app.get('*', (req, res) => res.sendFile(__dirname + '/build/index.html'));
+    server.listen({ port: config.port, host: '0.0.0.0' }, () => setlog(`Started HTTP service on port ${config.port}`));
+  } else {
+    setlog('Connection to MongoDB failed', loaded);
+  }
 });
