61d1ea2c47b6b757548ee01210497bfc3782223c
webgently <oleksandrshkabura811@gmail.com>
2023-05-09 17:17:54 +0300

    feat: add register router

--- src/routers.ts
+++ src/routers.ts
@@ -1,5 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { register } from './controllers/user';
 
 const routers = express.Router();
+
+routers.post('/register', register);
 
 export default routers;
